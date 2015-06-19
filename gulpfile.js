6770c73b3bdbbc3d83ec10c7358cc954412a060f
qianyj <qianyj@cairenhui.com>
2015-06-19 16:55:32 +0800

    图片展示

--- gulpfile.js
+++ gulpfile.js
@@ -8,8 +8,10 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),/*文件清理*/
     concat = require('gulp-concat'),/*拼接*/
     notify = require('gulp-notify'),
+    autoprefixer = require('gulp-autoprefixer'),
     cache = require('gulp-cache'),
     browserSync = require('browser-sync')/*即使刷新*/
+    runSequence = require('run-sequence')
 
 
 
@@ -37,19 +39,21 @@ gulp.task('browser-sync', function () {
 
 /*****************编译Sass，Autoprefix及缩小化**********/
 gulp.task('styles', function() {
-  return gulp.src('src/sass/main.scss')/**定义来源档案**/
+  return gulp.src('src/sass/*')/**定义来源档案**/
     .pipe(sass({ style: 'expanded' }))/**定义sass输出格式**/
-    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))/*浏览器前缀后处理*/
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(gulp.dest('dist/css'))/*gulp.destAPI设定目的路径*/
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/css'))
     .pipe(notify({ message: 'styles task complete' }));
 });
+gulp.task('default', ['styles', 'watch']);
+
 
 /*****************脚本编译*************************/
 gulp.task('scripts', function() {
-  return gulp.src('src/js/**/*.js')
+  return gulp.src('src/js/**/*')
     .pipe(jshint())
     .pipe(jshint.reporter('default'))
     .pipe(concat('main.js'))/*合并所有js到min.js*/
@@ -63,16 +67,22 @@ gulp.task('default', ['scripts', 'watch']);
 
 /*****************清除目的目录重建档案**********/
 gulp.task('clean', function() {
-  return gulp.src(['src/styles', 'src/scripts', 'src/images'], {read: false})
+  return gulp.src(['src/dist/css', 'src/dist/js'], {read: false})
     .pipe(clean());
 });
 
 /****************** 监测文档 *************************/
 gulp.task('watch', function() {
   // 看守所有.scss档
-  gulp.watch('src/css/**/*.scss', ['styles']);
+  gulp.watch('src/sass/**/*.scss', ['styles']);
   // 看守所有.js档
   gulp.watch('src/js/**/*.js', ['scripts']);
   // 看守所有图片档
   gulp.watch('src/images/**/*', ['images']);
+  gulp.watch('src/i*.html', ['html']);
 });
+
+/******************  task  *********************/
+gulp.task('default',function(){
+    runSequence('clean','browser-sync','scripts','watch')
+});  //顺序尽量和watch一致，且要html在css前
